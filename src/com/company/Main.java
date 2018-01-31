3ed7b24a8aa8e42c1682b130adc6ac57e2740c41
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-31 11:31:25 +0200

    Antra uzduotis

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc =  new Scanner(System.in);
+
+	String zodis = "";
+
+	while (!zodis.equals("pabaiga")){
+	    zodis = sc.next();
+
+	    System.out.println(zodis);
+    }
+    }
+}
